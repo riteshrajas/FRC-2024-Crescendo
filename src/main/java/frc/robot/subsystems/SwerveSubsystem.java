6661cbe8fd066d37d484f358d2ddc4dc1f52cdd4
charySHS <smkzachatac@gmail.com>
2024-02-15 20:43:48 -0800

    Reverted

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -5,6 +5,7 @@ import java.util.function.Supplier;
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 
@@ -72,23 +73,6 @@ public class SwerveSubsystem extends SwerveDrivetrain implements Subsystem {
     public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
         return run(() -> this.setControl(requestSupplier.get()));
     }
-
-    public Command driveForward() {
-        return SwerveRequest.RobotCentric().withDriveRequestType(SwerveModule.DriveRequestType.OpenLoopVoltage);
-    }
-
-    public Command stopChassis() {
-        return SwerveRequest.SwerveDriveBrake();
-    }
-
-    public Command point() {
-        return SwerveRequest.PointWheelsAt();
-    }
-
-    public Command aimRobot() {
-        return SwerveRequest.RobotCentric().withSteerRequestType(SwerveModule.SteerRequestType.MotionMagicExpo);
-    }
-
 
     public Command getAutoPath(String pathName) {
         return new PathPlannerAuto(pathName);
