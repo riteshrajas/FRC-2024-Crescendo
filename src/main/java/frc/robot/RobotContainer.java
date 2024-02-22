12b35f521ab6bc4ccf20cf90651c0c0982deb5f2
Josh <JLeyshock@kintsugiyama.com>
2024-02-21 21:38:01 -0800

    Cleaned up robot container, implemented lookup table

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,27 +5,24 @@
 package frc.robot;
 
 
-import org.photonvision.PhotonCamera;
-
-import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModule;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.*;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-
-//import frc.robot.Commands.TurnInDirectionOfTarget;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.IntakeSusbsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.subsystems.VisionSystems.LimelightVision;
+import friarLib2.math.LookupTable;
 import friarLib2.vision.LimelightCamera;
-import frc.robot.subsystems.IntakeSusbsystem;
+import org.photonvision.PhotonCamera;
 
 
 /**
@@ -36,181 +33,178 @@ import frc.robot.subsystems.IntakeSusbsystem;
  */
 public class RobotContainer
 {
+    // --------------------------------------------------------------------------------------------
     // -- Controllers
+    // --------------------------------------------------------------------------------------------
     public static CommandXboxController Driver = new CommandXboxController(0);
     public static CommandXboxController Operator = new CommandXboxController(1);
 
+    
 
+    // --------------------------------------------------------------------------------------------
+    // -- Tuning Values
+    // --------------------------------------------------------------------------------------------
     private double MaxSpeed = 2.11; // 6 meters per second desired top speed
     private double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
+    private LookupTable ThrottleLookup = LookupTable.CreateNormalized()
+            .AddValue(0.1, 0) // deadband
+            .AddValue(0.55, 0.15)
+            .AddValue(0.8, 0.4);
 
-    /* Setting up bindings for necessary control of the swerve drive platform */
-    public final SwerveSubsystem drivetrain = TunerConstants.DriveTrain; // My drivetrain
-
-    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-            .withDeadband(MaxSpeed * 0.2).withRotationalDeadband(MaxAngularRate * 0.2) // Add a 10% deadband
-            .withDriveRequestType(SwerveModule.DriveRequestType.OpenLoopVoltage); // I want field-centric
-    // driving in open loop
-    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-    private final SwerveRequest.RobotCentric forwardStraight = new SwerveRequest.RobotCentric().withDriveRequestType(SwerveModule.DriveRequestType.OpenLoopVoltage);
-    public final SwerveRequest.RobotCentric AimRobot = new SwerveRequest.RobotCentric().withSteerRequestType(SwerveModule.SteerRequestType.MotionMagicExpo);
-    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
-    private final Telemetry logger = new Telemetry(MaxSpeed);
-
-    // -- Camera's
-    private LimelightCamera shooterCamera = new LimelightCamera();
-    private PhotonCamera testCamera = new PhotonCamera("Test");
-
-    // -- Subsystems 
+    
+    // --------------------------------------------------------------------------------------------
+    // -- Subsystems
+    // --------------------------------------------------------------------------------------------
+    public final SwerveSubsystem drivetrain = TunerConstants.DriveTrain;
     private final ArmSubsystem Arm = new ArmSubsystem();
     private final IntakeSusbsystem Intake = new IntakeSusbsystem();
 
-    // -- Auto
-    // private SwerveAutoBuilder AutoBuilder = null;
-//    private final SendableChooser<Command> AutoChooser = new SendableChooser<>();
+
+    
+    // --------------------------------------------------------------------------------------------
+    // -- Cameras
+    // --------------------------------------------------------------------------------------------
+    private LimelightCamera shooterCamera = new LimelightCamera();
+    private PhotonCamera testCamera = new PhotonCamera("Test");
+    
+    
+    
+    // --------------------------------------------------------------------------------------------
+    // -- Drive requests
+    // --------------------------------------------------------------------------------------------
+    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric().withDriveRequestType(SwerveModule.DriveRequestType.OpenLoopVoltage);
+    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+    public final SwerveRequest.RobotCentric AimRobot = new SwerveRequest.RobotCentric().withSteerRequestType(SwerveModule.SteerRequestType.MotionMagicExpo);
+    private final Telemetry logger = new Telemetry(MaxSpeed);
+
+
+
+    // --------------------------------------------------------------------------------------------
+    // -- Auto Chooser
+    // --------------------------------------------------------------------------------------------
     private final SendableChooser<Command> autoChooser;
 
 
     public RobotContainer()
     {
         autoChooser = AutoBuilder.buildAutoChooser();
-        ConfigureBindings();
-        SetDefaultCommands();
-        // ConfigureSwerveAutoBuilder();
-        // ConfigureAutoCommands();
+        ConfigureAutoCommands();
         SmartDashboard.putData("Auto Chooser", autoChooser);
+        
+        SetDefaultCommands();
+        
+        ConfigureDriverBindings();
+        ConfigureOperatorBindings();
 
-
+        drivetrain.registerTelemetry(logger::telemeterize);
     }
 
-
-
+    
+    
+    private void ConfigureAutoCommands()
+    {
+        autoChooser.addOption("mid", drivetrain.getAutoPath("3 note middle") );   
+    }
+    
     public Command GetAutonomousCommand()
     {
         return autoChooser.getSelected();
     }
 
-
-    private void ConfigureBindings()
+    
+    
+    private void SetDefaultCommands()
     {
-        // ----------------------------------------------------------------------------------------
-        // -- Driver
-        // ----------------------------------------------------------------------------------------
+        drivetrain.setDefaultCommand(
+                drivetrain.applyRequest(() -> drive
+                        .withVelocityX(-ThrottleLookup.GetValue(Driver.getLeftY()) * MaxSpeed) // Drive forward with negative Y (forward)
+                        .withVelocityY(-ThrottleLookup.GetValue(Driver.getLeftX()) * MaxSpeed) // Drive left with negative X (left)
+                        .withRotationalRate(DefaultDriveRotationRate()) // Drive counterclockwise with negative X (left)
+                ).ignoringDisable(true));
+    }
 
+    
+    
+    // --------------------------------------------------------------------------------------------
+    // -- Driver
+    // --------------------------------------------------------------------------------------------
+    private void ConfigureDriverBindings()
+    {
+        Driver.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        Driver.b().whileTrue(drivetrain.applyRequest(() -> point.withModuleDirection(new Rotation2d(-Driver.getLeftY(), -Driver.getLeftX()))));
 
-        // -- Auto Turn
-
-
-//        new Trigger(OI.DriverLeft::getTrigger).whileTrue(new TurnInDirectionOfTarget(_Drive));
-
-        // Zero IMU
-//       new Trigger(OI.DriverLeft::getTop).onTrue(new InstantCommand(IMU::zeroIMU));
+        // reset the field-centric heading on left bumper press
+        Driver.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
 
         Driver.povUp().onTrue(Intake.Command_testInake(600));
         Driver.povUp().onFalse(Intake.Command_testInake(0));
+    }
+    
+    
+    
+    // --------------------------------------------------------------------------------------------
+    // -- Operator
+    // --------------------------------------------------------------------------------------------
+    private void ConfigureOperatorBindings()
+    {
 
-
-        // ----------------------------------------------------------------------------------------
-        // -- Operator
-        // ----------------------------------------------------------------------------------------
-
-        /********** Testing arm code **********/
+        // -- Testing arm code
         Operator.a().onTrue(Arm.Command_SetPosition(ArmSubsystem.EArmPosition.stowed)); //stowed
         Operator.b().onTrue(Arm.Command_SetPosition(ArmSubsystem.EArmPosition.shoot_subwoofer)); //shot speaker
         Operator.x().onTrue(Arm.Command_SetPosition(ArmSubsystem.EArmPosition.amp)); //amp
         Operator.y().onTrue(Arm.Command_SetPosition(ArmSubsystem.EArmPosition.trap)); //trap
 
-
-
-        /********** Intaking **********/
+        
+        // -- Intaking
 //        Operator.rightTrigger().whileTrue(
 //                Commands.parallel(
 //                    Arm.Command_SetPosition(ArmSubsystem.EArmPosition.stowed),
 //                    Intake.Command_SetPivotPosition(IntakeSusbsystem.EPivotPosition.intake))
 //                .andThen(Intake.Command_IntakeNote())
 //        );
-//
-//        Operator.rightTrigger().onFalse(Intake.Command_SetPivotPosition(IntakeSusbsystem.EPivotPosition.stowed));
+        
 
-        /********** Outtake **********/
-//        Operator.rightBumper().onTrue()
+        // Operator.rightTrigger().onFalse(Intake.Command_SetPivotPosition(IntakeSusbsystem.EPivotPosition.stowed));
 
+        // -- Outtake
+        // Operator.rightBumper().onTrue()
 
-        /********** Scoring **********/
-//       Operator.a().onTrue(Commands.parallel(Arm.Command_SetPosition(ArmSubsystem.EArmPosition.stowed), Intake.Command_SetPivotPosition(IntakeSusbsystem.EPivotPosition.stowed))); //stow
-//       Operator.b().onTrue(Commands.parallel(Arm.Command_SetPosition(ArmSubsystem.EArmPosition.shoot_subwoofer), Intake.Command_SetPivotPosition(IntakeSusbsystem.EPivotPosition.shoot_speaker))); //shoot speaker
-//       Operator.x().onTrue(Commands.parallel(Arm.Command_SetPosition(ArmSubsystem.EArmPosition.amp), Intake.Command_SetPivotPosition(IntakeSusbsystem.EPivotPosition.amp))); // shoot amp
-//       Operator.y().onTrue(Commands.parallel(Arm.Command_SetPosition(ArmSubsystem.EArmPosition.trap), Intake.Command_SetPivotPosition(IntakeSusbsystem.EPivotPosition.trap))); // place trap
+        // -- Scoring
+        // Operator.a().onTrue(Commands.parallel(Arm.Command_SetPosition(ArmSubsystem.EArmPosition.stowed), Intake.Command_SetPivotPosition(IntakeSusbsystem.EPivotPosition.stowed))); //stow
+        // Operator.b().onTrue(Commands.parallel(Arm.Command_SetPosition(ArmSubsystem.EArmPosition.shoot_subwoofer), Intake.Command_SetPivotPosition(IntakeSusbsystem.EPivotPosition.shoot_speaker))); //shoot speaker
+        // Operator.x().onTrue(Commands.parallel(Arm.Command_SetPosition(ArmSubsystem.EArmPosition.amp), Intake.Command_SetPivotPosition(IntakeSusbsystem.EPivotPosition.amp))); // shoot amp
+        // Operator.y().onTrue(Commands.parallel(Arm.Command_SetPosition(ArmSubsystem.EArmPosition.trap), Intake.Command_SetPivotPosition(IntakeSusbsystem.EPivotPosition.trap))); // place trap
 
-        /********** Climbing **********/
+        // -- Climbing
         Operator.leftStick().onTrue(Commands.parallel(Arm.Command_SetPosition(ArmSubsystem.EArmPosition.climb_firstpos), Intake.Command_SetPivotPosition(IntakeSusbsystem.EPivotPosition.stowed)));
         Operator.rightStick().onTrue(Commands.parallel(Arm.Command_SetPosition(ArmSubsystem.EArmPosition.climb_secondpos), Intake.Command_SetPivotPosition(IntakeSusbsystem.EPivotPosition.stowed)));
 
 
-        /********** Vision **********/
+        // -- Vision
         // Operator.back().onTrue(LimelightVision.SetPipelineCommand(0).ignoringDisable(true));
         Operator.start().onTrue(LimelightVision.SetPipelineCommand(1).ignoringDisable(true));
-//        Operator.rightStick().onTrue(LimelightVision.SetPipelineCommand(2).ignoringDisable(true));
+        // Operator.rightStick().onTrue(LimelightVision.SetPipelineCommand(2).ignoringDisable(true));
 
 
     }
 
+    
 
-    private void SetDefaultCommands()
+    private double DefaultDriveRotationRate()
     {
-        drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
-                drivetrain.applyRequest(() -> drive.withVelocityX(-Driver.getLeftY() * MaxSpeed) // Drive forward with
-                        // negative Y (forward)
-                        .withVelocityY(-Driver.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                        .withRotationalRate(-Driver.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
-                ).ignoringDisable(true));
-
-        Driver.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        Driver.b().whileTrue(drivetrain
-                .applyRequest(() -> point.withModuleDirection(new Rotation2d(-Driver.getLeftY(), -Driver.getLeftX()))));
-
-        // reset the field-centric heading on left bumper press
-        Driver.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
-
-        if (Utils.isSimulation()) {
-            drivetrain.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
+        if (Driver.getHID().getRightBumper())
+        {
+            var tx = NetworkTableInstance
+                    .getDefault()
+                    .getTable("limelight")
+                    .getEntry("tx")
+                    .getDouble(0);
+            
+             return tx * -0.1;
         }
-        drivetrain.registerTelemetry(logger::telemeterize);
-        Driver.povUp().whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0.5).withVelocityY(0)));
-        Driver.povUpRight().whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0.5).withVelocityY(-0.5)));
-        Driver.povUpLeft().whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0.5).withVelocityY(0.5)));
-        Driver.povDownRight().whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-0.5).withVelocityY(-0.5)));
-        Driver.povDownLeft().whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-0.5).withVelocityY(0.5)));
-        Driver.povDown().whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-0.5).withVelocityY(0)));
-        Driver.povRight().whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0).withVelocityY(-0.5)));
-        Driver.povLeft().whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0).withVelocityY(0.5)));
-        Driver.rightBumper().whileTrue(drivetrain.applyRequest(() -> AimRobot.withRotationalRate(LimelightVision.shooterCamera.getBestTarget().getX()))); // TODO: I don't know if this will work (if the robot has no target does it return 0?)
-        Driver.leftTrigger().whileTrue(drivetrain.applyRequest(() -> {
-            if (LimelightVision.shooterCamera.getBestTarget().getX() > 0.1) {
-                return AimRobot.withRotationalRate(LimelightVision.shooterCamera.getBestTarget().getX() * -1);
-            } else if (LimelightVision.getInchesFromTarget() > 5) {
-                return forwardStraight.withVelocityX(0).withVelocityY(0.5);
-            } else {
-                return forwardStraight.withVelocityX(0).withVelocityY(0);
-            }
-        }));
-        
-        //Driver.rightBumper().whileTrue(drivetrain.applyRequest(() -> AimRobot.withRotationalRate(testCamera)));
-
-
-//            Driver.leftBumper().whileTrue(drivetrain.applyRequest(() -> AimRobot.withRotationalRate(LimelightVision.shooterCamera.getBestTarget().getX()));
-
-
-
+        return -ThrottleLookup.GetValue(Driver.getRightX()) * MaxAngularRate;
     }
-
-
-    private void ConfigureAutos()
-    {
-        autoChooser.addOption("mid", drivetrain.getAutoPath("3 note middle") );
-    }
-
-
 }
 
 
